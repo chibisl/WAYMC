0950e9ef3834b49ba8e2986fd0a419d5550dd1f6
lsi <lsi@syneforge.com>
2018-05-29 12:41:05 +0300

    - remove ananlytics usage

--- android/src/ua/com/tlftgames/waymc/AndroidLauncher.java
+++ android/src/ua/com/tlftgames/waymc/AndroidLauncher.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.HitBuilders;
 
 public class AndroidLauncher extends AndroidApplication implements Tracker {
-    private com.google.android.gms.analytics.Tracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,35 +16,19 @@ public class AndroidLauncher extends AndroidApplication implements Tracker {
         initialize(new StaighremGame(this), config);
     }
 
-    private com.google.android.gms.analytics.Tracker getTracker() {
-        if (tracker == null) {
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            tracker = analytics.newTracker(R.xml.global_tracker);
-        }
-        return tracker;
-    }
-
     @Override
     public void trackScreen(String screenName) {
-        com.google.android.gms.analytics.Tracker t = getTracker();
-        t.setScreenName(screenName);
-        t.send(new HitBuilders.ScreenViewBuilder().build());
+
     }
 
     @Override
     public void trackEvent(String category, String action, String label, int value) {
-        com.google.android.gms.analytics.Tracker t = getTracker();
 
-        t.send(new HitBuilders.EventBuilder().setCategory(category).setAction(action).setLabel(label).setValue(value)
-                .build());
     }
 
     @Override
     public void trackNonInteractionEvent(String category, String action, String label, int value) {
-        com.google.android.gms.analytics.Tracker t = getTracker();
-
-        t.send(new HitBuilders.EventBuilder().setCategory(category).setAction(action).setLabel(label).setValue(value)
-                .setNonInteraction(true).build());
+    	
     }
 
 }
